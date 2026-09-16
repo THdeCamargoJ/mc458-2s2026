0fca6c6f2ceedc8bccba60717b1a8ad3dd366feb
thdecamargoj <thdecamargoj@gmail.com>
2026-09-16 20:46:54 -0300

    Got to O(n*log(n))

--- trabalho1/src/main.c
+++ trabalho1/src/main.c
@@ -14,8 +14,9 @@
  ******************************************************************************/
 
 #define MAX_AIRCRAFTS 10000000LL
-#define MAX_DOUBLE 100000000000LL
+#define INFTY __LONG_LONG_MAX__
 #define llint long long int
+#define square(x) ((x)*(x))
 
 
 typedef struct {
@@ -26,171 +27,29 @@ typedef struct {
 
 typedef struct Pair {
     llint aircraft_a, aircraft_b;
-    double distance;
-    struct Pair *next;
-    struct Pair *prev;
+    llint distance;
 } Pair;
 
 typedef struct {
-    Pair *head, *tail;
-    llint size;
-} Queue;
+    Pair *q;
+    int sorted;
+    llint size, max_size;
+} Stack;
 
 
 /******************************************************************************
- *                             EUCLIDEAN DISTANCE                             *
+ *                              MEMORY ALLOCATION                             *
  ******************************************************************************/
 
-double distance_between_aircrafts(const Aircraft a, const Aircraft b) {
-    return sqrt(
-        (double) ((a.pos_x - b.pos_x)*(a.pos_x - b.pos_x) 
-        + (a.pos_y - b.pos_y)*(a.pos_y - b.pos_y))
-    );
-}
-
-
-/******************************************************************************
- *                                    QUEUE                                   *
- ******************************************************************************/
-
-Queue *solution;
-
-void init_pair(const Aircraft a, const Aircraft b, Pair *pair) {
-    pair->aircraft_a = a.id;
-    pair->aircraft_b = b.id;
-
-    // Mantain i < j rule
-    if (a.id > b.id) {
-        pair->aircraft_a = b.id;
-        pair->aircraft_b = a.id;
-    }
-
-    pair->distance = distance_between_aircrafts(a, b);
-    pair->next = NULL;
-    pair->prev = NULL;
-
-}
-
-void print_pair(Pair *pair) {
-    llint id_i = pair->aircraft_a,
-          id_j = pair->aircraft_b;
-    
-    if (pair->aircraft_a > pair->aircraft_b) {
-        id_i = pair->aircraft_b;
-        id_j = pair->aircraft_a;
-    }
-
-    printf("(%lld,%lld)\n", id_i, id_j);
-}
-
-Queue *create_queue() {
-    Queue *queue = (Queue *) malloc(sizeof(Queue));
-    queue->head = NULL;
-    queue->tail = NULL;
-    queue->size = 0;
-    return queue;
-}
-
-void delete_queue(Queue *queue) {
-    Pair *pair, *aux;
-
-    if (queue->size) {
-        pair = queue->head;
-        while(pair != NULL) {
-            aux = pair->next;
-            free(pair);
-            pair = aux;
-        }
-    }
-
-    free(queue);
-}
-
-void enqueue_pair(Queue *queue, Pair *new_pair) {
-    Pair *pair, *p = queue->head;
-    
-    // Evitar duplicatas
-    while (p != NULL) {
-        if ((p->aircraft_a == new_pair->aircraft_a && p->aircraft_b == new_pair->aircraft_b)
-            || (p->aircraft_a == new_pair->aircraft_b && p->aircraft_b == new_pair->aircraft_a)) return;
-        p = p->next;
-    };
-
-    pair = malloc(sizeof(Pair));
-    *pair = *new_pair;
-
-    if (queue->size == 0) {
-        queue->head = pair;
-        queue->tail = pair;
-        queue->size = 1;
-        return;
-    }
-
-    queue->tail->next = pair;
-    pair->prev = queue->tail;
-    queue->tail = pair;
-    queue->size++;
-}
-
-int dequeue(Queue *queue, Pair *pair) {
-    Pair *aux;
-
-    if (queue->size == 0) {
-        return 1;
-    }
-
-    *pair = *queue->tail;
-
-    pair->next = NULL;
-    pair->prev = NULL;
-
-    if (queue->size == 1) {
-        free(queue->tail);
-        queue->head = NULL;
-        queue->tail = NULL;
-        queue->size = 0;
-        return 0;
-    }
-
-    aux = queue->tail->prev;
-    aux->next = NULL;
-    free(queue->tail);
-    queue->tail = aux;
-    queue->size--;
-    return 0;
-}
-
-llint get_queue_size(Queue *queue) {
-    if (queue == NULL) return -1;
-    return queue->size;
-}
-
-void clear_queue(Queue *queue) {
-    Pair *pair, *aux;
-
-    if (queue->size) {
-        pair = queue->head;
-        while(pair != NULL) {
-            aux = pair->next;
-            free(pair);
-            pair = aux;
-        }
-        queue->head = NULL;
-        queue->tail = NULL;
-        queue->size = 0;
-    }
-}
-
-void print_queue(Queue *queue) {
-    Pair *pair = queue->tail;
-    while (pair!=NULL) {
-        print_pair(pair);
-        pair = pair->prev;
-    }
+void mem_alloc(void *mem, llint n_items, size_t item_size) {
+    mem = malloc(n_items * item_size);
+    if (mem != NULL) return;
+    fprintf(stderr, "ERROR: malloc malfunction, could not reserve heap memory!\n");
+    exit(EXIT_FAILURE);
 }
 
 /******************************************************************************
- *                                  MERGESORT                                 *
+ *                                 COMPARATORS                                *
  ******************************************************************************/
 
 int compare_aircraft_x_positions(const void* item_a, const void* item_b) {
@@ -219,7 +78,7 @@ int compare_aircraft_y_positions(const void* item_a, const void* item_b) {
 
 int compare_pairs(const void *item_a, const void *item_b) {
     Pair *a = (Pair *) item_a, *b = (Pair *) item_b;
-    llint air_a_comparison = b->aircraft_a - a->aircraft_b,
+    llint air_a_comparison = b->aircraft_a - a->aircraft_a,
           air_b_comparison = b->aircraft_b - a->aircraft_b;
 
     // Compare y coordinates
@@ -233,19 +92,148 @@ int compare_pairs(const void *item_a, const void *item_b) {
     return 0; 
 }
 
+
+/******************************************************************************
+ *                             DISTANCE CALCULATION                           *
+ ******************************************************************************/
+
+llint distance_between_aircrafts(const Aircraft a, const Aircraft b) {
+    return (square(a.pos_x - b.pos_x) + square(a.pos_y - b.pos_y));
+}
+
+
+/******************************************************************************
+ *                                    STACK                                   *
+ ******************************************************************************/
+
+Stack *stack = NULL;
+
+void merge_sort(
+    void *vect,
+    size_t v_item_size,
+    llint size,
+    int (* compare_positions)(const void *, const void *)
+); // promisse for sorting the stack;
+
+void init_pair(const Aircraft a, const Aircraft b, Pair *pair) {
+    pair->aircraft_a = a.id;
+    pair->aircraft_b = b.id;
+
+    // Mantain i < j rule
+    if (a.id > b.id) {
+        pair->aircraft_a = b.id;
+        pair->aircraft_b = a.id;
+    }
+
+    pair->distance = distance_between_aircrafts(a, b);
+}
+
+void print_pair(Pair *pair) {
+    llint id_i = pair->aircraft_a,
+          id_j = pair->aircraft_b;
+    
+    if (pair->aircraft_a > pair->aircraft_b) {
+        id_i = pair->aircraft_b;
+        id_j = pair->aircraft_a;
+    }
+
+    printf("(%lld,%lld)\n", id_i, id_j);
+}
+
+void create_stack(llint size) {
+    stack = (Stack *) malloc(sizeof(Stack));
+    stack->q = malloc(size * sizeof(Pair));
+    stack->sorted = 0;
+    stack->max_size = size;
+    stack->size = 0;
+}
+
+llint get_stack_size() {
+    if (stack == NULL) return 0;
+    return stack->size;
+}
+
+void sort_stack() {
+    merge_sort(stack->q, sizeof(Pair), get_stack_size(), compare_pairs);
+    stack->sorted = 1;
+}
+
+void delete_stack() {
+    if (stack == NULL) return;
+    free(stack->q);
+    free(stack);
+}
+
+void stack_push(Pair new_pair) {
+    if (stack == NULL) return;
+
+    Pair pair, p = stack->q[0];
+    llint i = 0;
+
+    if (stack->size + 1 >= stack->max_size) return;
+    stack->q[stack->size] = new_pair;
+    stack->size++;
+}
+
+void stack_pop(Pair *pair) {
+    if (stack == NULL || stack->size == 0) return;
+    
+    *pair = stack->q[stack->size-1];
+    stack->size--;
+}
+
+void clean_stack_duplicates() {
+    if (stack == NULL) return;
+
+    if (get_stack_size() < 2) return;
+
+    if (!stack->sorted) sort_stack();
+
+    Pair *pairs = stack->q, pair_a, pair_b, aux;
+    llint uniques_size = 1;
+
+    pair_a = pairs[0];
+    for (llint i = 1; i < get_stack_size(); i++) {
+        pair_b = pairs[i];
+        if (compare_pairs(&pair_a, &pair_b) == 0) {
+            pairs[uniques_size - 1] = pair_b;
+        } else {
+            pairs[uniques_size++] = pair_b;
+            pair_a = pair_b;
+        }
+    }
+
+    stack->size = uniques_size;
+}
+
+void clear_stack() {
+    if (stack == NULL) return;
+    stack->size = 0;
+}
+
+void print_stack() {
+    if (stack == NULL) return;
+    llint i = 0;
+    while (i < stack->size) print_pair(&stack->q[i++]);
+}
+
+
+/******************************************************************************
+ *                                  MERGESORT                                 *
+ ******************************************************************************/
+
 void combine_merge_sort(
     void *v,
     size_t v_item_size,
     llint start, llint middle, llint end,
+    char *left_vector,
+    char *right_vector,
     int (* compare)(const void *, const void *)
 ) {
     char *vect = (char *)v;
-    char *left_vector, *right_vector, *left_item, *right_item;
+    char *left_item, *right_item;
     llint lv_i = 0, rv_i = 0, lv_size, rv_size, i;
     int comparison;
-
-    left_vector = malloc((end-start+1)*v_item_size);
-    right_vector = malloc((end-start+1)*v_item_size);
 
     for (i = start; i <= end; i++) {
         if (i <= middle) {
@@ -289,15 +277,14 @@ void combine_merge_sort(
         rv_i++;
         i++;
     }
-
-    free(left_vector);
-    free(right_vector);
 }
 
 void recursive_merge_sort(
     void *vect,
     size_t v_item_size,
     llint start, llint end,
+    char *left_vector,
+    char *right_vector,
     int (* compare)(const void *, const void *)
 ) {
     llint middle = start + (end - start) / 2;
@@ -305,11 +292,11 @@ void recursive_merge_sort(
     if (start >= end) return; // base
     
     // Divide
-    recursive_merge_sort(vect, v_item_size, start, middle, compare);
-    recursive_merge_sort(vect, v_item_size, middle + 1, end, compare);
+    recursive_merge_sort(vect, v_item_size, start, middle, left_vector, right_vector, compare);
+    recursive_merge_sort(vect, v_item_size, middle + 1, end, left_vector, right_vector, compare);
 
     // Conquer
-    combine_merge_sort(vect, v_item_size, start, middle, end, compare);
+    combine_merge_sort(vect, v_item_size, start, middle, end, left_vector, right_vector, compare);
 
 }
 
@@ -319,7 +306,13 @@ void merge_sort(
     llint size,
     int (* compare_positions)(const void *, const void *)
 ) {
-    recursive_merge_sort(vect, v_item_size,  0, size - 1, compare_positions);
+    char *left_vector = malloc((size/2+1)*v_item_size),
+         *right_vector = malloc((size/2+1)*v_item_size);
+
+    recursive_merge_sort(vect, v_item_size,  0, size - 1, left_vector, right_vector, compare_positions);
+
+    free(left_vector);
+    free(right_vector);
 }
 
 
@@ -327,53 +320,49 @@ void merge_sort(
  *                                  SOLUTION                                  *
  ******************************************************************************/
 
-void check_queue_min(Pair *pair) {
-    Pair queue_pair;
-    double pair_dist = pair->distance;
+void update_stack_min(Pair pair) {
+    Pair stack_pair;
+    llint pair_dist = pair.distance;
 
-    if (get_queue_size(solution) == 0) {
-        enqueue_pair(solution, pair);
+    if (get_stack_size() == 0) {
+        stack_push(pair);
         return;
     }
 
-    dequeue(solution, &queue_pair);
-    if (pair_dist > queue_pair.distance) {
-        enqueue_pair(solution, &queue_pair);
+    stack_pop(&stack_pair);
+    if (pair_dist > stack_pair.distance) {
+        stack_push(stack_pair);
         return;
     }
 
-    if (pair_dist < queue_pair.distance) {
-        clear_queue(solution);
-        enqueue_pair(solution, pair);
-        return;
-    }
+    stack_push(stack_pair);
+    if (pair_dist < stack_pair.distance) clear_stack();
 
-    enqueue_pair(solution, &queue_pair);
-    enqueue_pair(solution, pair);
+    stack_push(pair);
 }
 
-double find_partition_minimum_distance(Aircraft *partition, llint p_size, double prev_min_d) {
+llint find_partition_minimum_distance(Aircraft *partition, llint mid_x, llint p_size, llint prev_min_d) {
     Pair pair;
-    double min_dist = prev_min_d, distance;
+    llint min_dist = prev_min_d, oposite_size_aircrafts;
 
     merge_sort(partition, sizeof(Aircraft), p_size, compare_aircraft_y_positions);
 
     for (llint i = 0; i < p_size; i++)
-        for (llint j = i + 1; j < p_size
-                              && (partition[j].pos_y - partition[i].pos_y) < min_dist; j++) {
+        for (llint j = i + 1; j < p_size; j++) {
+            if (square(partition[j].pos_y - partition[i].pos_y) > min_dist) break;
+
             init_pair(partition[i], partition[j], &pair);
-            distance = distance_between_aircrafts(partition[i], partition[j]);
-            if (distance < min_dist) {
-                min_dist = distance;
+            if (pair.distance <= min_dist) {
+                min_dist = pair.distance;
+                update_stack_min(pair);
             }
-            check_queue_min(&pair);
         }
 
     return min_dist;
- }
+}
 
-double base_case_fmd(Aircraft *aircrafts, llint start, llint end) {
-    double min_dist = MAX_DOUBLE;
+llint base_case_fmd(Aircraft *aircrafts, llint start, llint end) {
+    llint min_dist = INFTY;
     Pair pair, queue_pair, min_pair;
 
     if (end - start + 1 == 1) return min_dist;
@@ -381,12 +370,12 @@ double base_case_fmd(Aircraft *aircrafts, llint start, llint end) {
     for (llint i = start; i < end; i++) {
         for (llint j = i + 1; j <= end; j++) {
             init_pair(aircrafts[i], aircrafts[j], &pair);
-            if (min_dist > pair.distance) {
+            if (min_dist >= pair.distance) {
                 min_dist = pair.distance;
                 min_pair = pair;
+                update_stack_min(min_pair);
             }
 
-            check_queue_min(&min_pair);
         }
     }
 
@@ -394,9 +383,9 @@ double base_case_fmd(Aircraft *aircrafts, llint start, llint end) {
 }
 
 
-double recursive_fmd(Aircraft *aircrafts, llint start, llint end) {
+llint recursive_fmd(Aircraft *aircrafts, llint start, llint end) {
     llint middle = start + (end - start) / 2, j, middle_x_pos = aircrafts[middle].pos_x;
-    double mld, mrd, md, pmd;
+    llint mld, mrd, md, pmd;
     Pair pair, pair_b, pair_c, queue_pair, min_pair;
     Aircraft *partition;
 
@@ -407,31 +396,30 @@ double recursive_fmd(Aircraft *aircrafts, llint start, llint end) {
     mld = recursive_fmd(aircrafts, start, middle);
     mrd = recursive_fmd(aircrafts, middle + 1, end);
 
-
     md = mld < mrd? mld : mrd;
 
     partition = malloc((end-start+1)*sizeof(Aircraft));
     j = 0;
     for (llint i = start; i <= end; i++) {
-        if (fabs(aircrafts[i].pos_x - middle_x_pos) < md) {
+        if (fabs(square(aircrafts[i].pos_x - middle_x_pos)) < md) {
             partition[j++] = aircrafts[i];
         }
     }
 
-    pmd = find_partition_minimum_distance(partition, j, md);
+    pmd = find_partition_minimum_distance(partition, middle_x_pos, j, md);
     free(partition);
     return md < pmd? md : pmd;
 }
 
 
-double find_minimum_distance(Aircraft *aircrafts, llint size) {
+llint find_minimum_distance(Aircraft *aircrafts, llint size) {
     return recursive_fmd(aircrafts, 0, size - 1);
 }
 
 void solve_for(Aircraft *aircrafts, llint n_aircrafts) {
     merge_sort(aircrafts, sizeof(Aircraft), n_aircrafts, compare_aircraft_x_positions);
     find_minimum_distance(aircrafts, n_aircrafts);
- }
+}
 
 
 /******************************************************************************
@@ -453,12 +441,13 @@ int main() {
         aircrafts[i].pos_y = y;
     }
 
-    solution = create_queue();
+    create_stack(n_aircrafts);
 
     solve_for(aircrafts, n_aircrafts);
-
-    print_queue(solution);
-    delete_queue(solution);
+    
+    clean_stack_duplicates();
+    print_stack();
+    delete_stack();
 
     free(aircrafts);
 
